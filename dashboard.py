ae47fce62ecdca478693c71b9e12695bf14e8300
RaghsX <raghavan.shashi@gmail.com>
2026-08-01 17:12:56 -0500

    Prepare dashboard for deployment

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,10 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_URL = "http://127.0.0.1:5000"
+API_URL = os.getenv(
+    "API_URL",
+    "http://127.0.0.1:5000"
+)
 
 st.set_page_config(
     page_title="Sales Analytics Dashboard",
